37604a7595bb50327d9e05ac46bdbf5413e2a0db
Egor <lldocll2012@outlook.com>
2023-07-11 19:41:11 +0300

    пытался что то сделать

--- example005_If_Else/Program.cs
+++ example005_If_Else/Program.cs
@@ -1,9 +1,10 @@
 ﻿Console.Write("Введите имя пользователя ");
 string username = Console.ReadLine();
-if(username.ToLower() == "настя")
+if(username.ToLower() == "настя");
 {
     Console.WriteLine("Привет, любимая, я так по тебе соскучился!!!!");
 }
+
 else
 {
     Console.Write("А, это ты. Привет, ");
